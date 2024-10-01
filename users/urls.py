c6c1d0f7b893729665e1a83def5c8c1579a891d1
Gabriel Linhares <gabriellr2301@outlook.com>
2024-10-01 15:35:11 -0300

    melhorando_interface_lista_filmes

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/', views.logout_view, name='logout'),
     # path('logout', views.logout_view,name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+def logout_view(request):
+    """Faz logout do usuario"""
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
 
 # Create your views here.
